8e1ece899e66dc794d10f2fb302f9491a502226c
vinomondi <vincentomondi1002@gmail.com>
2024-11-19 11:15:15 +0300

    feat: add main program entry point

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	lem "github.com/Vincent-Omondi/lem-in/funcs"
+)
+
+func main() {
+	lem.ValidData(lem.ValidArgs(os.Args))
+	validways := lem.Search()
+	if len(validways) == 0 || lem.Ants == 0 {
+		log.Fatal("ERROR: invalid data format")
+	}
+	os.Stdout.Write(lem.Graphoverview)
+	lem.Sendants(validways)
+}
