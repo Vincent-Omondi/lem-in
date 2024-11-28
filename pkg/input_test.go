27b126a151b9cbf99ec1bdd84543ac177b3b7eca
bernaotieno <bernadokumu1@gmail.com>
2024-11-28 11:51:53 +0300

    testing the functions in the input.go file

---
+++ pkg/input_test.go
@@ -0,0 +1,78 @@
+// input.go
+package pkg
+
+import (
+	"os"
+	"testing"
+)
+
+func TestProcessInputFile(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    struct{ inputFile *os.File }
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Valid file with start and end room",
+			args: struct{ inputFile *os.File }{
+				inputFile: createTestFile(t, "10\n##start\nroom1 0 0\n##end\nroom2 1 1\nroom1-room2\n"),
+			},
+			want:    "dfs",
+			wantErr: false,
+		},
+		{
+			name: "File with invalid special command",
+			args: struct{ inputFile *os.File }{
+				inputFile: createTestFile(t, "10\n##start\nroom1 0 0\n##invalid\nroom2 1 1\n"),
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "Missing room after ##start",
+			args: struct{ inputFile *os.File }{
+				inputFile: createTestFile(t, "10\n##start\n"),
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "Missing room after ##end",
+			args: struct{ inputFile *os.File }{
+				inputFile: createTestFile(t, "10\n##end\n"),
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "Duplicate connection between rooms",
+			args: struct{ inputFile *os.File }{
+				inputFile: createTestFile(t, "10\n##start\nroom1 0 0\n##end\nroom2 1 1\nroom1-room2\nroom1-room2\n"),
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "Room name starts with L",
+			args: struct{ inputFile *os.File }{
+				inputFile: createTestFile(t, "10\n##start\nLroom 0 0\n##end\nroom2 1 1\n"),
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ProcessInputFile(tt.args.inputFile)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ProcessInputFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ProcessInputFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
